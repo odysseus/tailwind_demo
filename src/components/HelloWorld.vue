8d816fce581ecd2a08074f873e336cb9fa5f9c25
Ryan Case <rmcase@gmail.com>
2025-09-25 14:17:35 -0600

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen grid place-items-center">
+    <div class="rounded-lg border-2 border-gray-300 px-6 py-3
+    transition-colors duration-300 hover:bg-blue-100">
+      <h1 class="text-4xl font-bold text-blue-600">Hello Tailwind v4</h1>
+    </div>
+  </div>
+</template>
